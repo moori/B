92256860de898ef1ac9a0342ac7c5da539d49681
moori <daniel.moori@gmail.com>
2023-05-07 19:20:52 -0300

    intro music

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -14,6 +14,7 @@ public class MenuController : MonoBehaviour
     public void OnClickStart()
     {
         loading.SetActive(true);
+        AudioManager.FadeOut();
         this.DelayAction(1f, () => {
             SceneManager.LoadScene("Game");
         });
